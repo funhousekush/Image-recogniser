e7128458bb407afb56f60a3b99a972732911dc60
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-06-07 20:38:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Webcam.set(
 {
-    height: 350,
+    height: 300,
     width: 300,
     image_format:"jpg",
     jpg_quality: 90,
@@ -20,4 +20,30 @@ function take_kushsnap()
     {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
     });
+}
+
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+console.log("ml5.version", ml5.version);
+function modelLoaded()
+{
+    console.log("Model Loaded!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
+
+function resultofkushsnap()
+{
+    img = document.getElementById("captured_image") 
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error("error!error!error!error!error!error!error!error!error!error!");
+    }
+    if(results)
+    {
+        console.log("result!");
+        document.getElementById("smart_span").innerHTML = results[0].label;
+        console.log(results);
+    }
 }
